88ac51a5019ce13f426716ffb8c50603848bf1f4
Raul Huertas <rax20037@gmail.com>
2026-07-21 22:27:46 -0500

    refactoring

--- app/src/main/java/com/example/mytestapplication/UIOptions.kt
+++ app/src/main/java/com/example/mytestapplication/UIOptions.kt
@@ -5,8 +5,10 @@ enum class OptionsType{
     List
 }
 class UIOptions{
+    //This is the name to display for this function
     private var displayName: String = ""
     //When this option is selected, the stack calls the executioner with 'internalName' as parameter
     private var internalName : String = ""
+    //If this option redirects us to antoher options, this should be filled with those options
     private var options: List<UIOptions> = emptyList()
 }
